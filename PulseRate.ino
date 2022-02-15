c0cefb70356ccf6cc169318a40c4747cf4d66f62
Ritwik Duggal <62705328+ritwikduggal@users.noreply.github.com>
2022-02-16 00:13:03 +0530

    Add files via upload

---
+++ PulseRate.ino
@@ -0,0 +1,20 @@
+int PulsePin = 0;
+int LED = 13;
+int Signal;
+int Threshold = 550;
+void setup() {
+  pinMode(13, OUTPUT);
+  Serial.begin(9600);  
+}
+
+void loop(){
+  Signal = analogRead(PulsePin);
+  Serial.println(Signal);
+  if(Signal > Threshold)
+  {
+    digitalWrite(LED, HIGH);
+  }
+  else {
+    digitalWrite(LED, LOW);
+  }
+}
---
+++ Sounds_Sensor_Analog.ino
@@ -0,0 +1,9 @@
+const int sound_input = A1;
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  Serial.println(analogRead(sound_input))0;
+
+}
